cbb38849d0a79cf732782a152f08985fbd13e684
RACHELI7AYALA <ayala12238@gmail.com>
2022-07-25 19:09:42 +0300

    data_visualization

--- Visualization/data_visualization.py
+++ Visualization/data_visualization.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import Utils.params as p
-from Data.datasets import split_data
+from Data import datasets as d
 
 df=pd.read_csv(p.csv_path)
 
@@ -23,18 +23,23 @@ def bar_samples_per_class(df):
 #display the split to train/test/validation using multy bar
 def display_split_train_test_validation(df):
 
-    train, validation, test = split_data(p.csv_path)
+    data = np.load(r"C:\Users\IMOE001\Downloads\data.npz")
+    y_train =pd.DataFrame(data['y_train'])
+    y_validation = pd.DataFrame(data['y_validation'])
+    y_test = pd.DataFrame(data['y_test'])
+
+    # train, validation, test = split_data(p.csv_path)
     labels = df['labels'].unique()
-    # Calculate optimal width
-    width = np.min(np.diff(labels)) / 20
+
+
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     # matplotlib 3.0 you have to use align
-    ax.bar(labels - width, train.groupby(p.labels_col_name_df)[p.images_col_name_df].count(), width, color='b',tick_label=labels, label='-Ymin', align='edge')
-    ax.bar(labels - width * 2, validation.groupby(p.labels_col_name_df)[p.images_col_name_df].count(), width, color='r',tick_label=labels, label='Ymax',
+    ax.bar(labels - p.width, y_train.groupby(0)[0].count(), p.width, color='b',tick_label=labels, label='-Ymin', align='edge')
+    ax.bar(labels , y_validation.groupby(0)[0].count(), p.width, color='r',tick_label=labels, label='Ymax',
            align='edge')
-    ax.bar(labels - width * 3, test.groupby(p.labels_col_name_df)[p.images_col_name_df].count(), width, color='g',tick_label=labels, label='-Ymin',
+    ax.bar(labels + p.width ,y_test.groupby(0)[0].count(), p.width, color='g',tick_label=labels, label='-Ymin',
            align='edge')
 
     ax.set_xlabel('train/validation/test')
@@ -43,14 +48,42 @@ def display_split_train_test_validation(df):
 
 #display some images for each class
 def display_data_example(df):
+
     labels = df[p.labels_col_name_df].unique()
+    fig = plt.figure(figsize=(100, 400),constrained_layout=True)
+    # setting values to rows and column variables
+    rows = 15
+    columns = 8
+    pos = 1
     for i in labels:
-        for j in df[df[p.labels_col_name_df]==i].head(5):
-             image = plt.imread(j[p.path_col_name_df])
-             plt.imshow(i,image)
+        ax = fig.add_subplot(rows, columns, pos)
+        ax.set_title(p.labels[int(i)],loc="left",size="small")
+        plt.axis('off')
+        for j in df[df[p.labels_col_name_df]==i].head(columns)[p.path_col_name]:
+
+             fig.add_subplot(rows, columns, pos)
+
+             image = plt.imread(j)
+             # showing image
+             plt.imshow(image)
+             plt.axis('off')
+             plt.subplot_tool()
+             plt.subplots_adjust(wspace=0, hspace=0.1)
+
+             # plt.subplots_adjust(left=0.1,
+             #                     bottom=0.1,
+             #                     right=0.9,
+             #                     top=0.9,
+             #                     wspace=0.4,
+             #                     hspace=2)
+             pos+=1
+    plt.show()
 
 
+def main():
+    bar_samples_per_class(df)
+    display_split_train_test_validation(df)
+    display_data_example(df)
 
-bar_samples_per_class(df)
-display_split_train_test_validation(df)
-display_data_example(df)
+if __name__=="__main__":
+    main()
